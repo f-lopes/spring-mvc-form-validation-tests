23c2c2a2793996e953e23ceb2e7dacc62397d7c2
Florian Lopes <florian.lopes@outlook.com>
2016-08-18 07:16:14 +0200

    Create Spring Boot project

---
+++ src/main/java/io/florianlopes/spring/SpringMvcFormValidationTestsApplication.java
@@ -0,0 +1,12 @@
+package io.florianlopes.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcFormValidationTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcFormValidationTestsApplication.class, args);
+	}
+}
